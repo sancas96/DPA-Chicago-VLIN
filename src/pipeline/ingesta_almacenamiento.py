96859b59f0081a82ee448ccc2c13573c4a341474
Vivis <visagu55@hotmail.com>
2021-02-21 04:02:00 -0600

    Checkpoint 2

--- src/pipeline/ingesta_almacenamiento.py
+++ src/pipeline/ingesta_almacenamiento.py
@@ -1,4 +1,47 @@
 from datetime import date, timedelta
+from sodapy import Socrata
+from src.utils.general import get_s3_credentials,get_api_token,read_yaml
+import pandas as pd
+import yaml
+import boto3
+import pickle
+import io
+
+def get_client():
+    token=get_api_token('conf/local/credentials.yaml')['api_token']
+    client = Socrata('data.cityofchicago.org',token)
+    return client
+
+def get_s3_resource():
+    s3_creds= get_s3_credentials('conf/local/credentials.yaml')
+    session = boto3.Session(
+    aws_access_key_id=s3_creds['aws_access_key_id'],
+    aws_secret_access_key=s3_creds['aws_secret_access_key'])
+    s3 = session.client('s3')
+    return s3
+
+def ingesta_inicial(client,limite):
+    datos=client.get('4ijn-s7e5', limit=limite)
+    datos_binario=io.BytesIO()
+    pickle.dump(datos, datos_binario)
+    datos_binario.seek(0)
+    return datos_binario
+    
+def ingesta_consecutiva(client,fecha,limite):
+    datos=client.get('4ijn-s7e5', limit=limite, where=f"inspection_date='{fecha}'")
+    datos_binario=io.BytesIO()
+    pickle.dump(datos, datos_binario)
+    datos_binario.seek(0)
+    return datos_binario
+
+def guardar_ingesta(data, bucket, bucket_path):
+    get_s3_resource().upload_fileobj(data, bucket, f"{bucket_path}{date.today()}.pkl")
+    pass
+
+
+'''
+#Código Ita
+from datetime import date, timedelta
 import pandas as pd
 from sodapy import Socrata
 import yaml
@@ -57,3 +100,4 @@ def upload_info_s3(initial_data = False):
         filename = ingesta_consecutiva()
         s3_key = f"ingestion/consecutive/{filename}"
     client.upload_file(filename,"data-product-architecture-equipo-8",s3_key)
+'''    
--- src/utils/general.py
+++ src/utils/general.py
@@ -1,4 +1,28 @@
 import yaml
+
+def read_yaml(credentials_file):
+    """ load yaml cofigurations """
+    config = None
+    try: 
+        with open (credentials_file, 'r') as f:
+            config = yaml.safe_load(f)
+    except:
+        raise FileNotFoundError('Couldnt load the file')
+    return config
+
+def get_s3_credentials(credentials_file):
+    s3_credentials=read_yaml(credentials_file)['s3']
+    return s3_credentials
+
+def get_api_token(credentials_file):
+    token=read_yaml(credentials_file)['food_inspections']
+    return token
+
+
+'''
+#Código Ita 
+
+import yaml
 import boto3
 
 
@@ -16,5 +40,4 @@ def get_s3_client():
         aws_secret_access_key=aws_secret_access_key
     )
     return client
-
-
+'''
